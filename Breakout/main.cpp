2972352a85ef19c3cf491edd4cff496b83d0da86
Susmit Agrawal <susmit@susmits-macbook-pro.local>
2019-03-28 14:16:17 +0530

    Initial Commit

---
+++ Breakout/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Breakout
+//
+//  Created by Susmit Agrawal on 28/03/19.
+//  Copyright © 2019 Susmit Agrawal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
